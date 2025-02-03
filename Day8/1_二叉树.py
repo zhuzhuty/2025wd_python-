aba7904f340610d1bb98ef073b8c4048302f8dd8
JC <junchengzhu5@Outlook.com>
2025-02-03 16:54:20 +0800

    btree

---
+++ Day8/1_二叉树.py
@@ -0,0 +1,17 @@
+"""
+Created on 2025-02-03
+"""
+
+
+# Your code starts here
+class Node:
+    def __init__(self, elem=-1, lchild=None, rchild=None):
+        self.elem = elem
+        self.lchild = lchild
+        self.rchild = rchild
+
+class BTree:
+    pass
+
+if __name__ == '__main__':
+    pass
